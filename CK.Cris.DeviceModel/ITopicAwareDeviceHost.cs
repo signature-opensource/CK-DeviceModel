f83ab9643ce6188cc105a960c85c5f220f81bb9f
Dan Chiche <dan.chiche@signature.one>
2025-01-21 16:43:56 +0100

    Transfert from CK-DeviceModel-ByTopic => CK.Cris.DeviceModel  and  CK.IO.DeviceModel
    CK.IO.DeviceModel
     - Change ICommandDeviceTargetTopic by ICommandDeviceTopics and TargetTopics => Topics
    
    CK.Cris.DeviceModel
     - ITopicTargetAwareDeviceHost => ITopicAwareDeviceHost
     - Validators change and re-adapt with lists

---
+++ CK.Cris.DeviceModel/ITopicAwareDeviceHost.cs
@@ -0,0 +1,26 @@
+using CK.Core;
+using CK.IO.DeviceModel;
+
+namespace CK.Cris.DeviceModel;
+
+[IsMultiple]
+public interface ITopicAwareDeviceHost
+{
+
+    /// <summary>
+    /// A device host name is "DeviceHostTypeName".
+    /// This can be all possible devices host. Example: "LEDStripHost" or "SignatureDeviceHost"
+    /// </summary>
+    string DeviceHostName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="monitor"></param>
+    /// <param name="cmd"></param>
+    /// <returns>
+    /// Returns true if the command has been handled, false if the command is not handled by this device.
+    /// This throws on error.
+    /// </returns>
+    ValueTask<bool> HandleAsync( IActivityMonitor monitor, ICommandDeviceTopics cmd );
+}
---
+++ CK.DeviceModel/StandardColor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.DeviceModel;
+public enum StandardColor
+{
+    Off,
+    WhiteBlinking,
+    White,
+    RedBlinking,
+    Red,
+    GreenBlinking,
+    Green,
+    BlueBlinking,
+    Blue,
+    YellowBlinking,
+    Yellow,
+    MagentaBlinking,
+    Magenta,
+    CyanBlinking,
+    Cyan,
+}
+
