9d7a6b2a78b9c5fb04b49e81cdf0cb81c6329255
Dan Chiche <dan.chiche@signature.one>
2025-01-21 16:51:03 +0100

    Add StandardColorExtensions

--- CK.DeviceModel/StandardColor.cs
+++ CK.DeviceModel/StandardColor.cs
@@ -24,3 +24,14 @@ public enum StandardColor
     Cyan,
 }
 
+public static class StandardColorExtensions
+{
+    /// <summary>
+    /// Gets whether this color is blinking.
+    /// </summary>
+    /// <param name="color">This color.</param>
+    /// <returns>True if this color is blinking, false otherwise.</returns>
+    public static bool IsBlinking( this StandardColor color ) => ((int)color & 1) != 0;
+}
+
+
