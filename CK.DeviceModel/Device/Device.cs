d95430a17e4b412b0a836dda6845c11afa232b5e
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-03-16 13:34:35 +0100

    Always enqueue StartCommand.
    Removed the sync pre-check  on _isRunning.
    This doesn't change anything in concurrent scenarii but enables
    start after an automatic stop (due for instance to an unhandled error)
    in the same activity to be handled correctly (no pull/retry that could
    not really be safely done anyway).
    
    .

--- CK.DeviceModel/Device/Device.cs
+++ CK.DeviceModel/Device/Device.cs
@@ -615,11 +615,6 @@ public abstract partial class Device<TConfiguration> : BaseDevice, IDevice, IInt
             await HandleStartAsync( null, DeviceStartedReason.SelfStart ).ConfigureAwait( false );
             return _isRunning;
         }
-        var preCheck = SyncStateStartCheck( monitor );
-        if( preCheck.HasValue )
-        {
-            return preCheck.Value;
-        }
         var cmd = _host.CreateStartCommand( Name );
         if( !UnsafeSendCommand( monitor, cmd ) )
         {
@@ -629,33 +624,23 @@ public abstract partial class Device<TConfiguration> : BaseDevice, IDevice, IInt
         return await cmd.Completion.Task.ConfigureAwait( false );
     }
 
-    bool? SyncStateStartCheck( IActivityMonitor monitor )
-    {
-        if( _configStatus == DeviceConfigurationStatus.Disabled )
-        {
-            monitor.Error( $"Device {FullName} is Disabled by configuration." );
-            return false;
-        }
-        if( _isRunning )
-        {
-            monitor.Debug( "Already running." );
-            return true;
-        }
-        return null;
-    }
-
     async Task HandleStartAsync( BaseStartDeviceCommand? cmd, DeviceStartedReason reason )
     {
         using( _commandMonitor.OpenInfo( $"Starting {FullName} ({reason})" ).ConcludeWith( () => _isRunning ? "Success." : "Failed." ) )
         {
-            var check = SyncStateStartCheck( _commandMonitor );
-            if( check.HasValue )
+            if( _configStatus == DeviceConfigurationStatus.Disabled )
             {
-                cmd?.Completion.SetResult( check.Value );
+                _commandMonitor.Error( $"Device {FullName} is Disabled by configuration." );
+                cmd?.Completion.SetResult( false );
+                return;
+            }
+            if( _isRunning )
+            {
+                _commandMonitor.Debug( "Already running." );
+                cmd?.Completion.SetResult( true );
                 return;
             }
             Exception? error = null;
-            Debug.Assert( _isRunning == false );
             try
             {
                 if( await DoStartAsync( _commandMonitor, reason ).ConfigureAwait( false ) )
@@ -679,7 +664,9 @@ public abstract partial class Device<TConfiguration> : BaseDevice, IDevice, IInt
             // Sets the completion last.
             if( cmd != null )
             {
-                bool complete = error != null ? cmd.Completion.TrySetException( error ) : cmd.Completion.TrySetResult( _isRunning );
+                bool complete = error != null
+                                    ? cmd.Completion.TrySetException( error )
+                                    : cmd.Completion.TrySetResult( _isRunning );
                 if( !complete )
                 {
                     _commandMonitor.Warn( $"Command has been completed outside of the normal process." );
